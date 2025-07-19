49eb2d4d6a4a0971cdf2a1943200c231d3789309
pranamg <pranamg@users.noreply.github.com>
2025-07-19 06:42:53 +0000

    Add DuckDB CLI installation script and command examples

---
+++ ch02/duckdb_cli_install.sh
@@ -0,0 +1,11 @@
+# DuckDB CLI Installation (Linux)
+
+To install the DuckDB CLI on Linux:
+
+wget https://github.com/duckdb/duckdb/releases/download/v1.3.2/duckdb_cli-linux-amd64.zip
+unzip duckdb_cli-linux-amd64.zip
+chmod +x duckdb
+./duckdb -version
+sudo mv ./duckdb /usr/local/bin/
+
+duckdb
